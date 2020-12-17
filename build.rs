a222d298325dc529b014e5f6e5dee67752226863
metherul <metherul@protonmail.com>
2020-12-17 10:13:08 -0500

    removed target link statement

--- build.rs
+++ build.rs
@@ -17,5 +17,4 @@ fn main() {
         .status();
 
     println!("cargo:rustc-link-search=minhook/cmake/Debug/");
-    // println!("cargo:rustc-link-lib=[static]minhook/cmake/Debug/minhook.x64d");
 }
